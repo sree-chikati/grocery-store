dc8c81af805f9a28cbb9666615dc007db6f68e20
sree-chikati <srechi453.fusdk12@gmail.com>
2021-02-09 13:29:50 -0800

    Updated models to show Store Titles

--- grocery_app/models.py
+++ grocery_app/models.py
@@ -19,6 +19,9 @@ class GroceryStore(db.Model):
     address = db.Column(db.String(200), nullable=False)
     items = db.relationship('GroceryItem', back_populates='store')
 
+    def __str__(self):
+        return str(self.title)
+
 class GroceryItem(db.Model):
     """Grocery Item model."""
     id = db.Column(db.Integer, primary_key=True)
@@ -26,6 +29,5 @@ class GroceryItem(db.Model):
     price = db.Column(db.Float(precision=2), nullable=False)
     category = db.Column(db.Enum(ItemCategory), default=ItemCategory.OTHER)
     photo_url = db.Column(URLType)
-    store_id = db.Column(
-        db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
     store = db.relationship('GroceryStore', back_populates='items')
