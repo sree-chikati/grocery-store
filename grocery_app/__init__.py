e565f0e78e640898135fb8f87dfa877351ac4a66
sree-chikati <srechi453.fusdk12@gmail.com>
2021-02-16 22:51:21 -0800

    HW4: Adding Routes

--- grocery_app/__init__.py
+++ grocery_app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from grocery_app.config import Config
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
@@ -8,10 +10,21 @@ app.config.from_object(Config)
 app.secret_key = os.urandom(24)
 
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-from grocery_app.routes import main
+from .models import User
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+bcrypt = Bcrypt(app)
+
+from grocery_app.routes import main, auth
 
 app.register_blueprint(main)
+app.register_blueprint(auth)
 
 with app.app_context():
     db.create_all()
--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
-from grocery_app.models import GroceryStore, GroceryItem
-from grocery_app.forms import GroceryStoreForm, GroceryItemForm
+from grocery_app.models import GroceryStore, GroceryItem, User
+from grocery_app.forms import GroceryStoreForm, GroceryItemForm, SignUpForm, LoginForm, AddItemForm
+from flask_login import login_user, logout_user, login_required, current_user
+from grocery_app import bcrypt
 from grocery_app import app, db
 
 main = Blueprint("main", __name__)
@@ -15,6 +17,7 @@ def homepage():
     all_stores = GroceryStore.query.all()
     print(all_stores)
     return render_template('home.html', all_stores=all_stores)
+
 
 @main.route('/new_store', methods=['GET', 'POST'])
 def new_store():
@@ -32,6 +35,7 @@ def new_store():
         return redirect(url_for('main.store_detail', store_id=new_store.id))
    
     return render_template('new_store.html', form=form)
+
 
 @main.route('/new_item', methods=['GET', 'POST'])
 def new_item():
@@ -54,6 +58,7 @@ def new_item():
 
     return render_template('new_item.html', form=form)
 
+
 @main.route('/store/<store_id>', methods=['GET', 'POST'])
 def store_detail(store_id):
     store = GroceryStore.query.get(store_id)
@@ -69,6 +74,7 @@ def store_detail(store_id):
         return redirect(url_for('main.store_detail', store_id=store.id, store=store))
 
     return render_template('store_detail.html', store=store, form=form)
+
 
 @main.route('/item/<item_id>', methods=['GET', 'POST'])
 def item_detail(item_id):
@@ -89,4 +95,49 @@ def item_detail(item_id):
         return redirect(url_for('main.item_detail', item_id=item.id, item=item))
 
     return render_template('item_detail.html', item=item, form=form)
+
+
+##########################################
+#         Authentication  Routes         #
+##########################################
+
+auth = Blueprint("auth", __name__)
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    print('in signup')
+    form = SignUpForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(
+            username=form.username.data,
+            password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('Account Created.')
+        print('created')
+        return redirect(url_for('auth.login'))
+    print(form.errors)
+    return render_template('signup.html', form=form)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=True)
+            next_page = request.args.get('next')
+            return redirect(next_page if next_page else url_for('main.homepage'))
+    return render_template('login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.homepage'))
+
 
